7d53a98c0dee2ae6e82af5ab018aa8660395fb22
Andressa Anverze <andressaanverze@gmail.com>
2020-08-19 22:00:02 -0300

    Primeiro programa java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+		public static void main(String[] args)
+		{
+			System.out.print("Esta eh uma saudacao: ");
+			System.out.println("Hello, world!");
+			System.out.printf("Idade: %d\n", 20);
+		}
+}
+
+//java Hello.java - complica para bytecode (.class)
+//java Hello - execuita o arquivo .class
